5ca6fd34b37348968ae0dcee33e13c4556001b0a
Edgardlt <edgardlt34@gmail.com>
2018-08-22 00:58:31 -0500

    I create a function that checks if the entered key is a letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,8 @@
 var bands = ['the killers','coldplay','one republic'];
 //This variable safe a random band from the array
 var secretBand = bands[Math.floor(Math.random() * bands.length)];
-
+//In letters we are saving all the letters that have been clicked so we don´t repeat
+var letters = [];
 
 function isLetter(str) {
   if (str.length === 1 && str.match(/[a-z]/i)){
@@ -12,13 +13,21 @@ function isLetter(str) {
     };
 };
 
+function saveLetter(entrada){    
+    if(isLetter(entrada) && letters.indexOf(entrada) <  0){
+        letters.push(entrada);
+        console.log(letters);
+    }else if(letters.indexOf(entrada) >= 0){
+        console.log("You already enter that letter");
+    }else{
+        console.log("You didn´t enter a valid key");
+    }
+};
+
 document.onkeyup = function(event) {
     var letter = event.key.toLowerCase();
 
-    if(isLetter(letter)){
-
-    }
-
-
+    saveLetter(letter);
+    
 };
 
